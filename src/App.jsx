18cf19b0ee1406ee12e5495b0272467a1aefa01b
Gaurish17Sharma <gaurishsharma03@gmail.com>
2024-03-09 16:14:05 +0530

    Add index.html node_modules/ package-lock.json package.json public/ src/ vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+import Search from './Components/Search';
+import Present_weather from './Components/Present_weather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
+      })
+      .catch(console.log);
+  };
+
+  console.log(currentWeather);
+  console.log(forecast);
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      <Present_weather />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.jsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": '6470337a6bmsh61056d0738bd8f7p12ac90jsn7f887d762378',
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = '77375896558c068ed821aefa94a3a199';
+
